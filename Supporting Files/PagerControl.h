089ba61832bdca986b5616f44dd2b43e5f3ae071
Maxim Smirnov <atimca@gmail.com>
2018-01-22 14:51:07 +0300

    initial commit

---
+++ Supporting Files/PagerControl.h
@@ -0,0 +1,19 @@
+//
+//  PagerControl.h
+//  PagerControl
+//
+//  Created by Maxim Smirnov on 22/01/2018.
+//  Copyright © 2018 Maxim Smirnov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PagerControl.
+FOUNDATION_EXPORT double PagerControlVersionNumber;
+
+//! Project version string for PagerControl.
+FOUNDATION_EXPORT const unsigned char PagerControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PagerControl/PublicHeader.h>
+
+
