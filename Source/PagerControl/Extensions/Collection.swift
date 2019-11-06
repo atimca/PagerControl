45b2f2fe52cd6e2da110ba36fce417288fad7eaf
Maxim Smirnov <>
2019-11-06 11:53:02 +0100

    publicity was removed from subscript(safe:) for Collection

--- Source/PagerControl/Extensions/Collection.swift
+++ Source/PagerControl/Extensions/Collection.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public extension Collection {
+extension Collection {
     
     /// Returns the element at the specified index iff it is within bounds, otherwise nil.
     subscript (safe index: Index) -> Iterator.Element? {
